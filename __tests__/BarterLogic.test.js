a175a2a6a2d318353966edcdcd5c270485c5fcf7
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-07-04 20:52:43 +1200

    Add tests for evaluating propositions where one participant doesn't trade any items.

--- __tests__/BarterLogic.test.js
+++ __tests__/BarterLogic.test.js
@@ -1,8 +1,22 @@
+
+const createValuedItem = require('../BarterLogic/createValuedItem');
 const createProposition = require('../BarterLogic/createProposition')
 const evaluateProposition = require('../BarterLogic/evaluateProposition');
+
+const offer1 = [
+    createValuedItem('A', 'a', 50),
+    createValuedItem('B', 'b', 205),
+    createValuedItem('C', 'c', 167)
+]
 
 test('Empty proposition should have value 0.', () => {
     expect(evaluateProposition(createProposition([], []))).toBe(0);
 })
 
-test('')
+test("If an NPC gives no items, value should be value of the player's items.", () =>{
+    expect(evaluateProposition(createProposition(offer1, []))).toBe(422);
+});
+
+test("If player gives no items, value should be negative the NPC's items.", () =>{
+    expect(evaluateProposition(createProposition([], offer1))).toBe(-422);
+});
