0ee553df0ec9b89f465101ed3954d8e16b9891b8
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-07-04 21:03:40 +1200

    Add further proposition evaluation tests.
    To test that two offers of the same value will cause the proposition's value to be 0, and that evalutions are generally correct,

--- __tests__/BarterLogic.test.js
+++ __tests__/BarterLogic.test.js
@@ -8,6 +8,12 @@ const offer1 = [
     createValuedItem('B', 'b', 205),
     createValuedItem('C', 'c', 167)
 ]
+const offer2 = [
+    createValuedItem('A', 'a', 156),
+    createValuedItem('B', 'b', 8),
+    createValuedItem('C', 'c', 12),
+    createValuedItem('D', 'd', 59)
+];
 
 test('Empty proposition should have value 0.', () => {
     expect(evaluateProposition(createProposition([], []))).toBe(0);
@@ -19,4 +25,13 @@ test("If an NPC gives no items, value should be value of the player's items.", (
 
 test("If player gives no items, value should be negative the NPC's items.", () =>{
     expect(evaluateProposition(createProposition([], offer1))).toBe(-422);
+});
+
+test("Propositions are evaluated correctly.", () => {
+    expect(evaluateProposition(createProposition(offer1, offer2))).toBe(187);
+    expect(evaluateProposition(createProposition(offer2, offer1))).toBe(-187);
+});
+
+test("If two offers have the same value, the proposition value should be 0", () => {
+    expect(evaluateProposition(createProposition(offer2, [createValuedItem('E', 'e', 235)]))).toBe(0);
 });
