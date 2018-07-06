f20ecd9b3540edaaa5e7852a7736c6ad3590ee37
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-07-06 13:32:36 +1200

    Create test to ensure addPlayerItemToProposition adds the correct item to the given proposition.

--- __tests__/BarterLogic.test.js
+++ __tests__/BarterLogic.test.js
@@ -2,6 +2,7 @@
 const createValuedItem = require('../BarterLogic/createValuedItem');
 const createProposition = require('../BarterLogic/Propositions/createProposition')
 const evaluateProposition = require('../BarterLogic/evaluateProposition');
+const addPlayerItemToProposition = require('../BarterLogic/Propositions/addPlayerItemToProposition');
 
 const offer1 = [
     createValuedItem('A', 'a', 50),
@@ -15,15 +16,17 @@ const offer2 = [
     createValuedItem('D', 'd', 59)
 ];
 
+
+//evaluateProposition
 test('Empty proposition should have value 0.', () => {
     expect(evaluateProposition(createProposition([], []))).toBe(0);
 })
 
-test("If an NPC gives no items, value should be value of the player's items.", () =>{
+test("If an NPC gives no items, value should be value of the player's items.", () => {
     expect(evaluateProposition(createProposition(offer1, []))).toBe(422);
 });
 
-test("If player gives no items, value should be negative the NPC's items.", () =>{
+test("If player gives no items, value should be negative the NPC's items.", () => {
     expect(evaluateProposition(createProposition([], offer1))).toBe(-422);
 });
 
@@ -34,4 +37,19 @@ test("Propositions are evaluated correctly.", () => {
 
 test("If two offers have the same value, the proposition value should be 0", () => {
     expect(evaluateProposition(createProposition(offer2, [createValuedItem('E', 'e', 235)]))).toBe(0);
+});
+
+
+//addPlayerItemToProposition
+test('Given item is added to the given proposition.', () => {
+    
+    /* Generate expected output: A proposition with 'N' added to the end of the player's offer. */
+    const expectedPlayerOffer = Array.from(offer1);
+    expectedPlayerOffer.push(createValuedItem('N', 'n', 120));
+
+    const expectedProposition = createProposition(expectedPlayerOffer, offer2);
+
+
+    expect(addPlayerItemToProposition(createValuedItem('N', 'n', 120), createProposition(offer1, offer2), () => true)).toBe(expectedProposition);
+
 });
