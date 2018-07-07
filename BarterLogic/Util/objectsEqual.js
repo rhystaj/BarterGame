22ba4d400dcd30c8c2666f1021782c501a7d2020
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-07-07 13:34:01 +1200

    Change to handle strings as a special case to avoid endless recursion.

--- BarterLogic/Util/objectsEqual.js
+++ BarterLogic/Util/objectsEqual.js
@@ -5,21 +5,28 @@
  */
 function objectsEqual(a, b){
     
+    if(a === b) {return true}
+
+    
+    /* If both are strings, but not equal, this function would just endlessly recurse unless explicity told to stop. */
+    if(typeof(a) === 'string' && typeof(b) === 'string'){
+        return false
+    }
+
+
     /* Get Keys from both objects, if there are any */
     const aKeys = Object.keys(a);
     const bKeys = Object.keys(b);
+    console.log("[" + aKeys.length + " - " + bKeys.length + "]");
 
 
-    if(aKeys.length !== bKeys.length) return false;
-
-
-    /* If Object.keys doesn't produce any keys, it is not a JSON object, so just use regular equals. */
-    if(aKeys.length > 0) return a === b;
+    if(aKeys.length !== bKeys.length) { return false; }
+    if(aKeys.length <= 0) { return false }
 
 
     /* Compare each of the values in the objects, and determine that objects are equals, if all thier key values are equal. */
     aKeys.forEach(key => {
-        if(!deepEquals(a[key], b[key])) return false;
+        if(!objectsEqual(a[key], b[key])) { return false; }
     });
     return true;
 
